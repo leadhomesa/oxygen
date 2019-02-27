f53a45d4df63ce38bdc5934930982d0ded8dc921
Kyle Opperman <kyleopperman1@gmail.com>
2019-02-27 09:04:31 +0200

    Fix build

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,13 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import babel from 'rollup-plugin-babel'
-import reactSvg from "rollup-plugin-react-svg";
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import reactSvg from 'rollup-plugin-react-svg';
 
-import pkg from './package.json'
+import pkg from './package.json';
 
 export default {
   input: 'src/index.js',
-  external: ['styled-components'],
+  external: ['styled-components', 'react', 'react-dom', 'prop-types'],
   globals: { 'styled-components': 'styled' },
   plugins: [
     reactSvg(),
@@ -34,4 +34,4 @@ export default {
       format: 'umd'
     }
   ]
-}
+};
