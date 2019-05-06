31e6ec0646727719178c19a378c8f6a40b89aa40
Seagyn Davis <seagyn.davis@leadhome.co.za>
2019-05-06 16:16:52 +0200

    refactor button component to pass remainder of props (Thanks Kyle!)

--- src/button/index.js
+++ src/button/index.js
@@ -3,15 +3,12 @@ import StyledButton from './style';
 import PropTypes from 'prop-types';
 import Loading from './loading.svg';
 
-const Button = props => {
-  const { children, loading } = props;
-  return (
-    <StyledButton {...props}>
-      {children}
-      {loading && <Loading />}
-    </StyledButton>
-  );
-};
+const Button = ({ children, loading, ...rest }) => (
+  <StyledButton {...rest}>
+    {children}
+    {loading && <Loading />}
+  </StyledButton>
+);
 
 Button.propTypes = {
   color: PropTypes.oneOf(['primary', 'secondary', 'warning', 'success']),
