384118054130abcec32f5df4e88061b34c02e10e
Seagyn Davis <seagyn.davis@leadhome.co.za>
2019-05-16 07:53:52 +0200

    CI: inject git branch to Docker

--- .buildkite/ci.sh
+++ .buildkite/ci.sh
@@ -4,4 +4,4 @@
 set -euxo pipefail
 
 docker build -t oxygen .
-docker run -e GH_TOKEN=${GH_TOKEN} -e NPM_TOKEN=${NPM_TOKEN} oxygen
+docker run -e GH_TOKEN=${GH_TOKEN} -e NPM_TOKEN=${NPM_TOKEN} -e GIT_BRANCH={BUILDKITE_BRANCH} oxygen
