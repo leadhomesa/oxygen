d450b2112afceb8fe8d0947ca094462fb094b23e
Francois Laubscher <francois.laubscher@leadhome.co.za>
2019-05-20 15:59:53 +0200

    fix(build): default export was broken
    fix(build): default export was broken

--- src/index.js
+++ src/index.js
@@ -1,25 +1,8 @@
 import Button from './button';
-
 import Colors from './colors';
-
 import Input, { Currency as InputCurrency } from './input';
 import Dropdown from './dropdown';
-
 import Header from './header';
 import Footer from './footer';
 
-const Oxygen = {
-  Button,
-
-  Colors,
-
-  Input,
-  InputCurrency,
-
-  Dropdown,
-
-  Header,
-  Footer
-};
-
-export default Oxygen;
+export { Button, Colors, Input, InputCurrency, Dropdown, Header, Footer };
