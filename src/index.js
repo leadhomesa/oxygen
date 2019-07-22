17aaa6d987959facb6ec36d557467055cdb846e2
Seagyn Davis <seagyn.davis@leadhome.co.za>
2019-07-22 15:56:28 +0200

    fix(propertycard): export card components

--- src/index.js
+++ src/index.js
@@ -6,9 +6,13 @@ import Header from './header';
 import Footer from './footer';
 import AutoComplete from './autocomplete';
 import Toast from './toast';
+import CardBody from './card-body/index';
+import CardFooter from './card-footer/index';
 
 export {
   Button,
+  CardBody,
+  CardFooter,
   Colors,
   Input,
   InputCurrency,
