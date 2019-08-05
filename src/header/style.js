ca016a85a38ed05fcfa4096a5b13df5a671eff6b
Francois Laubscher <francoislaubscher@outlook.com>
2019-08-05 12:47:29 +0200

    header uses small logo on mobile and links to '/'

--- src/header/style.js
+++ src/header/style.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 import colors from '../colors';
 
+import { Link } from 'react-router-dom';
+import { ReactComponent as LeadhomeSVG } from '../../assets/leadhome-logo.svg';
+import { ReactComponent as SmallLeadhomeSVG } from '../../assets/leadhome-logo-small.svg';
+
 export const HeaderContainer = styled.div`
   display: flex;
   flex-direction: row;
@@ -9,4 +13,24 @@ export const HeaderContainer = styled.div`
   margin-left: auto;
   background-color: ${colors.darkStorm};
   color: ${colors.white};
+`;
+
+// this hides the Leadhome text on mobile
+export const LeadhomeLogo = styled(LeadhomeSVG)`
+  @media (min-width: 320px) {
+    display: none;
+  }
+  @media (min-width: 1024px) {
+    display: block;
+  }
+`;
+
+export const SmallLeadhomeLogo = styled(SmallLeadhomeSVG)`
+  @media (min-width: 1024px) {
+    display: none;
+  }
+`;
+
+export const StyledLink = styled(Link)`
+  margin-right: 1rem;
 `;
