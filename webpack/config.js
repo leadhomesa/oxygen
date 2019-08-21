143f5f90292f5aa2ff01192802c6c4f4295af2d4
Francois Laubscher <francoislaubscher@outlook.com>
2019-08-21 14:41:04 +0200

    update deps and remove styled-components as a dependency

--- webpack/config.js
+++ webpack/config.js
@@ -30,22 +30,30 @@ module.exports = {
   },
   resolve: {
     alias: {
-      'react': path.resolve(__dirname, './node_modules/react'),
-      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+      react: path.resolve(__dirname, 'node_modules', 'react'),
+      'react-dom': path.resolve(__dirname, 'node_modules', 'react-dom'),
+      'styled-components': path.resolve(
+        __dirname,
+        'node_modules',
+        'styled-components'
+      )
     }
   },
   externals: {
     react: {
       commonjs: 'react',
       commonjs2: 'react',
-      amd: 'React',
-      root: 'React'
+      amd: 'React'
     },
     'react-dom': {
       commonjs: 'react-dom',
       commonjs2: 'react-dom',
-      amd: 'ReactDOM',
-      root: 'ReactDOM'
+      amd: 'ReactDOM'
+    },
+    'styled-components': {
+      commonjs: 'styled-components',
+      commonjs2: 'styled-components',
+      amd: 'styled-components'
     }
   }
 };
