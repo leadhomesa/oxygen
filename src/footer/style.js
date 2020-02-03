9a2ee45640aa5e74ebe716896767db88bc8457ef
Seagyn Davis <seagyn.davis@leadhome.co.za>
2020-02-03 17:24:31 +0200

    refactor: 💡 First attempt at babel only

--- src/footer/style.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const FooterContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 100%;
-  position: fixed;
-  bottom: 0;
-`;
--- src/pill/index.js
+++
@@ -1,8 +0,0 @@
-import { PropTypes } from 'prop-types';
-import Pill from './style';
-
-Pill.propTypes = {
-  children: PropTypes.string.isRequired
-};
-
-export default Pill;
